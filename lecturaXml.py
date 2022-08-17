1d8c9cba93d246e8e715e4053f3610e78bf94503
414ND1N <414nd1n@gmail.com>
2022-08-16 20:40:21 -0600

    Lectura archivo XML

---
+++ lecturaXml.py
@@ -0,0 +1,25 @@
+from xml.dom import minidom
+
+#ruta = input("Ingrese la ruta del documento\n")
+ruta = "E:\Mis Documents\Programacion\Python\IPC2\LAB\proyecto 1\Tarea1.xml"
+doc = minidom.parse(ruta)
+
+pacientes = doc.getElementsByTagName("paciente")
+
+for paciente in pacientes:
+    
+    nombre = paciente.getElementsByTagName("nombre")[0]
+    edad = paciente.getElementsByTagName("edad")[0]
+    periodos = paciente.getElementsByTagName("periodos")[0]
+    m = paciente.getElementsByTagName("m")[0]
+    celdas = paciente.getElementsByTagName("celda")
+
+    print("Nombre: %s " % nombre.firstChild.data)
+    print("Edad: %s " % edad.firstChild.data)
+    print("Periodos: %s" % periodos.firstChild.data)
+    print("m: %s" % m.firstChild.data)
+
+    for celda in celdas:
+        print("Celda f: %s, c: %s" %(celda.getAttribute("f"),celda.getAttribute("c")))
+
+    print("")    
